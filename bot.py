0efa6d25c9b08e5d9dabfd11fac792bf4b4de659
Ba1Kerrr <daniildavidov2707@gmail.com>
2024-04-09 15:03:18 +0300

    fix parce and bot to work together

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
 import telebot;
+from parce import search_price
 bot = telebot.TeleBot('6434260055:AAE7JMZQZ45ijJAO2xYPChMnu3N35lhJ8Dk')
+@bot.message_handler(commands=['start','help'])
+def basic_2(message):
+    bot.send_message(message.from_user.id, "Привет , Я бот для обмена криптовалютой.Я могу обменивать bitcoin и ethereum, если ты хочешь начать  обменивать валюту - просто напиши ее название")
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text in ("Привет","привет"):
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
+    elif message.text == "Bitcoin":
+        bot.send_message(message.from_user.id,search_price)
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 bot.polling(none_stop=True, interval=0)
--- parce.py
+++ parce.py
@@ -1,9 +1,11 @@
 import decimal
 import requests
+import time
 from bs4 import BeautifulSoup
-
-url = 'https://coinmarketcap.com/currencies/bitcoin/'
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
-search_price = soup.find('span', class_='sc-f70bb44c-0 jxpCgO base-text').text[1:].replace(',','')
-search_price = decimal.Decimal(search_price)
+while True:
+    url = 'https://www.binance.com/ru/price/bitcoin'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+    search_price = soup.find('div', class_='css-1bwgsh3').text[1:].replace(',', '')
+    search_price = decimal.Decimal(search_price)
+    print(f'Курс Bitcoin - {search_price}')
