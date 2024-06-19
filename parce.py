55f8740c7e4533c5566e278fd1cf975a39b3c45e
Ba1Kerrr <daniildavidov2707@gmail.com>
2024-06-19 23:47:09 +0300

    cleared from rubbish

--- parce.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-while 1:
-    url_bit = 'https://www.binance.com/ru/price/bitcoin'
-    page_bit = requests.get(url_bit)
-    soup_bit = BeautifulSoup(page_bit.text, "html.parser")
-    search_price_bit = soup_bit.find('div', class_='css-1bwgsh3').text[1:].replace(',', '')
-    last_bit = float(search_price_bit)
